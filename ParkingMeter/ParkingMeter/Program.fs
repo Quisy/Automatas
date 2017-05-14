93bccce889cc879b0bd30d1f0a493ee88baf51d2
Quisy <lamprecht.mariusz@gmail.com>
2017-05-14 12:38:47 +0200

    Added ParkingMeter WPF F# App

---
+++ ParkingMeter/ParkingMeter/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Markup
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv = 
+    let mainWindow = Application.LoadComponent(
+                        new System.Uri("/ParkingMeter;component/Views/MainView.xaml", UriKind.Relative)) :?> Window
+ 
+    let application = new Application()
+    application.Run(mainWindow)
