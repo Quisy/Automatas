37703bff14509f596a3c24ea3c50a8ca6622735c
Quisy <lamprecht.mariusz@gmail.com>
2017-05-14 19:50:17 +0200

    Added stateRoad

--- ParkingMeter/ParkingMeter/ViewModels/MainViewModel.fs
+++ ParkingMeter/ParkingMeter/ViewModels/MainViewModel.fs
@@ -5,10 +5,11 @@ open Models
 open System.Windows.Input
 open ViewModels.BaseViewModel
 
-type MainViewModel() = 
+type MainViewModel() as mainVM = 
     inherit BaseViewModel()
     let mutable _value : int = 0
     let mutable _state : ParkingMeterState = ParkingMeterState.Q0
+    let mutable _visitedSates : List<ParkingMeterState> = [ ParkingMeterState.Q0 ]
     
     let createCommand action canExecute = 
         let event1 = Event<_, _>()
@@ -17,6 +18,8 @@ type MainViewModel() =
               member this.Execute(obj) = action (obj)
               member this.add_CanExecuteChanged (handler) = event1.Publish.AddHandler(handler)
               member this.remove_CanExecuteChanged (handler) = event1.Publish.AddHandler(handler) }
+    
+    let addToList (list : List<ParkingMeterState>) (element : ParkingMeterState) = element :: list
     
     member this.Value 
         with get () = _value
@@ -30,26 +33,45 @@ type MainViewModel() =
             _state <- v
             base.OnPropertyChanged(<@ this.State @>)
     
+    member this.StateRoad = _visitedSates |> List.fold (fun string s -> " > " + s.ToString() + string) ""
     member this.AddToValue(value : int) = this.Value <- this.Value + value
-    member this.AddOne = createCommand (fun _ -> this.AddToValue(1) |> this.manageState()) (fun _ -> true)
-    member this.AddTwo = createCommand (fun _ -> this.AddToValue(2) |> this.manageState()) (fun _ -> true)
-    member this.AddFive = createCommand (fun _ -> this.AddToValue(5) |> this.manageState()) (fun _ -> true)
-    member private this.manageState() = 
+    
+    member this.AddOne = 
+        createCommand (fun _ -> 
+            this.AddToValue(1)
+            this.manageState()) (fun _ -> true)
+    
+    member this.AddTwo = 
+        createCommand (fun _ -> 
+            this.AddToValue(2)
+            this.manageState()) (fun _ -> true)
+    
+    member this.AddFive = 
+        createCommand (fun _ -> 
+            this.AddToValue(5)
+            this.manageState()) (fun _ -> true)
+    
+    member private this.manageState = 
         let currentValue = this.Value
         match currentValue with
-        | 1 -> fun () -> this.State <- ParkingMeterState.Q1
-        | 2 -> fun () -> this.State <- ParkingMeterState.Q2
-        | 3 -> fun () -> this.State <- ParkingMeterState.Q3
-        | 4 -> fun () -> this.State <- ParkingMeterState.Q4
-        | 5 -> fun () -> this.State <- ParkingMeterState.Q5
-        | 6 -> fun () -> this.State <- ParkingMeterState.Q6
+        | 1 -> fun () -> this.changeState (ParkingMeterState.Q1)
+        | 2 -> fun () -> this.changeState (ParkingMeterState.Q2)
+        | 3 -> fun () -> this.changeState (ParkingMeterState.Q3)
+        | 4 -> fun () -> this.changeState (ParkingMeterState.Q4)
+        | 5 -> fun () -> this.changeState (ParkingMeterState.Q5)
+        | 6 -> fun () -> this.changeState (ParkingMeterState.Q6)
         | currentValue when this.Value = 7 -> 
-            fun () -> 
-                this.State <- ParkingMeterState.Q7
+            fun _ -> 
+                this.changeState (ParkingMeterState.Q7)
                 this.Value <- 0
-        | currentValue when this.Value > 7-> 
+        | currentValue when this.Value > 7 -> 
             fun () -> 
-                this.State <- ParkingMeterState.Q8
+                this.changeState (ParkingMeterState.Q8)
                 this.Value <- 0
         | _ -> fun () -> this.State <- ParkingMeterState.Q0
-        
+    
+    member private this.changeState = 
+        fun (state : ParkingMeterState) -> 
+            this.State <- state
+            _visitedSates <- addToList _visitedSates state
+            mainVM.OnPropertyChanged(<@ this.StateRoad @>) |> ignore
